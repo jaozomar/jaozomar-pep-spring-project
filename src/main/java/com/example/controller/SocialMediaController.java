9a9560978a8d30478d3862265020a6f8ccdeb733
Joshua Ozomaro <ozomaroj@gmail.com>
2025-01-17 19:32:34 +0000

    finished user registration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,15 @@
 package com.example.controller;
 
+import com.example.entity.*;
+import com.example.repository.AccountRepository;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+import org.springframework.http.ResponseEntity;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +17,27 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController // accounts for @ResponseBody annotation
 public class SocialMediaController {
+    AccountService accountService;
+    MessageService messageService;
+    
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
 
+    @PostMapping("/register")
+    public ResponseEntity registerAccount(@RequestBody Account account) {
+        Optional<Account> result = accountService.registerAccount(account);
+
+        //failed because of empty or short username
+        if(result == null)
+            return ResponseEntity.status(400).body(account);
+        else if(result.isEmpty()) // failed because of duplicate
+            return ResponseEntity.status(409).body(account);
+        else // return account with updated id if succesfully persisted
+            return ResponseEntity.status(200).body(result);
+    }
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,34 @@
 package com.example.service;
 
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Optional<Account> registerAccount(Account acc) {
+        if(acc.getUsername() == null || acc.getUsername().length() < 4)
+            return null;
+        
+        // check if account with username exists
+        Optional<Account> result = accountRepository.findAccountByUsername(acc.getUsername());
+
+        // add account if it does not exist, and get the account again so that you have the updated id
+        if(result.isEmpty()) {
+            accountRepository.save(acc);
+            result = accountRepository.findAccountByUsername(acc.getUsername());
+        } else
+            result = Optional.empty(); //empty the optional if it already existed, so that we return an empty optional
+
+        return result;
+    }
 }
